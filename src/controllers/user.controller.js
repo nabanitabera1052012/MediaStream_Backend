b1898700bca710bbcccd118b5fdf3dd3cafd852a
nabanitabera1052012 <nabanitabera1052012@gmail.com>
2026-02-25 21:23:26 +0530

    add subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -189,7 +189,7 @@ const registerUser =asyncHandler(async(req,res) =>{
     })
     const refreshAccessToken = asyncHandler(async(req,res) =>{
         const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
-        if(incomingRefreshToken){
+        if(!incomingRefreshToken){
             throw new ApiError(401,"Refresh token is required")
         }
         try {
